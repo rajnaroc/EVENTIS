189dc00da2c02787c6134df70e63db9705805461
Aaron Campos <aaroncm611@gmail.com>
2025-05-04 07:16:58 +0200

    acabado el header y footer de la pagina

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, app, request, jsonify
+from flask import Flask, app, render_template
 
 
 
 app = Flask(__name__)
 
-
+@app.route('/', methods=['GET'])
+def inicio():
+    return render_template('inicio.html')
 
 if __name__ == '__main__':
     app.run(debug=True)  
