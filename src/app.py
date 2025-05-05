84ca31ca4b628d2f20670e67398858296bc6e68d
Aaron Campos <aaroncm611@gmail.com>
2025-05-05 05:30:40 +0200

    2.0.15: creando la pagina 404 y que funcione

--- src/app.py
+++ src/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, app, render_template, request
+from flask_login import LoginManager,login_user, login_required, logout_user, current_user
+from flask_mysqldb import MySQL
+# importaciones de los .py
 from config import config
 from forms import loginform, registerForm
+from entities import ModelUser
 
 app = Flask(__name__)
+db = MySQL(app)
+login_manager = LoginManager()
+
+@login_manager.user_loader
+def load_user(id):
+    return ModelUser.get_by_id(db, id)
 
 @app.route('/', methods=['GET'])
 def inicio():
@@ -21,6 +31,10 @@ def register():
     register = registerForm()
     return render_template('register.html', register=register)
 
+def status_404(error):
+    return render_template('404.html')
+
 if __name__ == '__main__':
     app.config.from_object(config["dev"])
+    app.register_error_handler(404, status_404)
     app.run()
