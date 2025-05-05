688447e1bc033728ee22241e53f5e88ec4cedad5
Aaron Campos <aaroncm611@gmail.com>
2025-05-05 02:50:58 +0200

    1.4.9: le pasamos los inputs a las vistas

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
+from math import log
 from flask import Flask, app, render_template
 from config import config
-
+from forms import loginform, registerForm
 
 app = Flask(__name__)
 
@@ -10,11 +11,13 @@ def inicio():
 
 @app.route('/iniciarsesion', methods=['GET'])
 def iniciar_sesion():
-    return render_template('iniciar_sesion.html')
+    login = loginform()
+    return render_template('iniciar_sesion.html', login=login)
 
 @app.route('/registrarse', methods=['GET'])
 def register():
-    return render_template('register.html')
+    register = registerForm()
+    return render_template('register.html', register=register)
 
 if __name__ == '__main__':
     app.config.from_object(config["dev"])
